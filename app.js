5ab7d82fd763d50237c787b5b321500960fad1e2
cintyy <cintiapaezdm@gmail.com>
2022-06-22 12:03:56 -0300

    ok

---
+++ app.js
@@ -0,0 +1,5 @@
+const primergusto = prompt('Elija un sabor');
+const segundogusto = prompt('Elija un sabor');
+const tercergusto = prompt('Elija un sabor');
+
+alert(`Aquí tiene su helado de ' + ${primergusto} + ${segundogusto} + ${tercergusto} +  '  ¡Muchas gracias por su compra!'`)
